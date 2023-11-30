28c2dc6201ba9e804cbf6c8a0f1aefa19897d201
ksskyy <149976697+ksskyy@users.noreply.github.com>
2023-11-30 15:35:16 -0800

    add banner and room part but still need to fix it

--- script/script.js
+++ script/script.js
@@ -1,6 +1,53 @@
-const btnClose = document.querySelector('.btn-close');
-const video = document.getElementById('video');
+const btnClose = document.querySelector(".btn-close");
+const video = document.getElementById("video");
 
-btnClose.addEventListener('click', () => {
-      video.pause();
+btnClose.addEventListener("click", () => {
+  video.pause();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  function initSlider() {
+    if (window.innerWidth > 576) {
+      $(".logo-slider").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 5,
+            },
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 4,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+            },
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 2,
+              arrows: false,
+            },
+          },
+        ],
+      });
+    }
+  }
+
+  initSlider();
+
+  window.addEventListener("resize", function () {
+    if ($(".logo-slider").hasClass("slick-initialized")) {
+      $(".logo-slider").slick("unslick");
+    }
+    initSlider();
+  });
 });
